4676312a3f69921701de5362e1eb8dd3e784674b
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2016-11-11 16:02:03 +0100

    Updated some aliases

--- alias-wopata.zsh
+++ alias-wopata.zsh
@@ -1,12 +1,12 @@
 # Projects folders
 alias woupee='~/Documents/woupee/repo'
-alias legrand='~/Documents/legrand/repo/Legrand\ ecat'
-alias mcs='~/Documents/mcs/repo/MonCoachSommeil'
-alias merck='~/Documents/merck/repo'
 alias appwm='~/Documents/appworldmix/repo'
+alias euralis='~/Documents/euralis-ios/'
+alias megaselfie='~/Documents/megaselfie/megaselfie/'
+alias mindful='~/Documents/mindful/repo/'
 
 # SSH to the CI machine
-alias bamboo='ssh bambooci@bamboo'
+alias bamboo='ssh bambooci@panda.local'
 
 # Updates the localized strings
 function istr()
@@ -19,6 +19,3 @@ function istr()
         echo "Neither 'update-localizations.js' nor 'poolstrings.js' exists" && false
     fi
 }
-
-# Removes all the old crashlytics bundles
-alias clear_fabric_archives='rm -rf ~/Library/Caches/com.crashlytics.mac'
