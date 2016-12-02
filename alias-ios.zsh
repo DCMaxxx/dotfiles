472484d04d79e6ae55d994f9d115f83dd2d86ab0
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2016-12-02 10:42:42 +0100

    Updated the alias xcode_fix_windows

--- alias-ios.zsh
+++ alias-ios.zsh
@@ -8,7 +8,7 @@ alias qprj='killall Xcode ; osascript -e "quit app \"GitUp\""'
 alias xcode_fix='killall Xcode && rm -rf ~/Library/Developer/Xcode/DerivedData ; open /Applications/Xcode.app'
 
 # Run it to reset a project's window configuration, if Xcode crashes when you open it
-alias xcode_fix_windows='rm ./*{.xcworkspace,.xcodeproj}/xcuserdata/*.xcuserdatad/UserInterfaceState.xcuserstate'
+alias xcode_fix_windows='rm -f ./*.xcworkspace/xcuserdata/*.xcuserdatad/UserInterfaceState.xcuserstate ; rm -f ./*.xcworkspace/xcuserdata/*.xcuserdatad/UserInterfaceState.xcuserstate'
 
 # Run it if for some reason, cocoapods takes forever to install
 alias cocoapods_fix='bundle exec pod repo remove master && bundle exec pod setup && bundle exec pod install'
