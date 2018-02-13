e1b861268a0dbe0f3195d62e2e93f792bfd01b52
Maxime de Chalendar <m.de-chalendar@betclicgroup.com>
2018-02-13 14:16:05 +0100

    Updated the 'clean' alias to keep Swift-generated temporary files

--- alias-emacs.zsh
+++ alias-emacs.zsh
@@ -6,6 +6,6 @@ function clean()
     if [ -z "$1" ]; then
 	      clean '.'
     else
-        find $1 \( -name "*~" -or -name ".*~" \) -exec rm -fv {} \;
+        find $1 -type f \( \( -name "*~" -or -name ".*~" \) -and \( -not -name "dgph~" -and -not -name "*.swiftdeps~" \) \) -exec rm -fv {} \;
     fi
 }
