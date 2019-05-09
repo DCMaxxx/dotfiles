8fc51d7dcb1b07492e1d90346fa22a5e7a563037
Maxime de Chalendar <m.de-chalendar@betclicgroup.com>
2019-05-09 12:56:18 +0200

    Refactored alias git

--- alias-git.zsh
+++ alias-git.zsh
@@ -1,9 +1,9 @@
-# Moves the last X un-pushed commits to a new branch
-function git_move_last_commits_to_new_branch()
+function git_rebase()
 {
-    if [ "$#" -ne 2 ]; then
-	     echo "usage: git_move_last_commits_to_new_branch branch_name nubmer_of_commits"
-    else
-	     git branch $1 && git reset --hard HEAD~$2 && git checkout $1
-    fi
+	git fetch origin && git checkout "$1" && git rebase "origin/${2:-develop}"
+}
+
+function git_reset_to_remote()
+{
+	git fetch origin && git reset --hard origin/`git branchname`
 }
