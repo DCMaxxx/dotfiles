2d8da51e3ca232f0308bdd7e9006720913d7aa27
Maxime de Chalendar <maxime.dechalendar@heetch.com>
2022-03-23 17:59:25 +0100

    Update the deploy script

--- alias-heetch.zsh
+++ alias-heetch.zsh
@@ -4,12 +4,17 @@ function deploy() {
   branch="`git rev-parse --abbrev-ref HEAD`"
   workflow="${1:-to-qa}"
 
-  read -q "CONFIRM?Triggering '${workflow}' for '${branch}'. Continue ? (y/n) "
+  read -q "CONFIRM?Triggering '${workflow}' for '${branch}'. Continue? (y/n) "
   echo ""
 
   if [ ${CONFIRM} = 'y' ]; then
-    build_number=`echo curl -s https://app.bitrise.io/app/$BITRISE_APP_ID/build/start.json --data "{\"hook_info\":{\"type\":\"bitrise\",\"build_trigger_token\":\"$BITRISE_BUILD_TRIGGER_TOKEN\"},\"build_params\":{\"branch\":\"${branch}\",\"workflow_id\":\"${workflow}\"},\"triggered_by\":\"curl\"}" | grep -Eo '\"build_number\"\:[0-9]+' | cut -d : -f2`
-    echo "Done. Build number is #${build_number}."
+    build_number=`curl -s https://app.bitrise.io/app/$BITRISE_APP_ID/build/start.json --data "{\"hook_info\":{\"type\":\"bitrise\",\"build_trigger_token\":\"$BITRISE_BUILD_TRIGGER_TOKEN\"},\"build_params\":{\"branch\":\"${branch}\",\"workflow_id\":\"${workflow}\"},\"triggered_by\":\"curl\"}" | grep -Eo '\"build_number\"\:[0-9]+' | cut -d : -f2`
+    echo -n "Done. Build number is #${build_number}. "
+    read -q "CONFIRM?Copy build number to clipboard? (y/n) "
+    echo ""
+    if [ ${CONFIRM} = 'y' ]; then
+      echo -n "#${build_number}" | pbcopy
+    fi
   else
     echo "Cancelled by user."
   fi
